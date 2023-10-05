061bca6c2b1247d407edcf0650778f5eee97cf00
Dmitry-Shikerin <130661730+Dmitry-Shikerin@users.noreply.github.com>
2023-10-05 15:18:22 +0300

    Добавьте файлы проекта.

---
+++ Венгерская нотация/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Венгерская_нотация
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int armySize = 10;
+            int coinsNumber = 10;
+            string name = "Vladislav";
+        }
+    }
+}
